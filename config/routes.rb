129e981c6652ba289e043e615b08b49f1ca35db4
Mario Augusto <tkyakow@gmail.com>
2017-06-28 22:24:01 -0300

    ajustes nas rotas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
   resources :daily_goals
   resources :goals
   resources :stores
   devise_for :users
+
+	devise_scope :user do
+	  root to: "devise/sessions#new"
+	end
 end
